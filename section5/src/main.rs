e8ce4dee9e7cc0e8c4cbc08877ec6cb11b59b40a
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-05 09:36:14 +0100

    if let statment

--- section5/src/main.rs
+++ section5/src/main.rs
@@ -50,7 +50,23 @@ fn main() {
     println!("{:?}", six);
     println!("{:?}", none);
 
-    what_pet("Dog")
+    what_pet("Dog");
+
+    let dog2 = Some(Pet::cat);
+    if let Some(Pet::dog) = dog2 {
+        println!("The animal is a dog")
+    } else {
+        println!("Not a dog")
+    };
+
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!("{}", top);
+    }
 }
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
