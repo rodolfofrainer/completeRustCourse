a454b73d44f4cded96bda46ab97bec7e384e3914
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-06 11:02:46 +0100

    section 6 - Generics

---
+++ section6/src/main.rs
@@ -0,0 +1,14 @@
+// Generic struct
+// struct Point<T, U> {
+//     x: T, //i32
+//     y: U, //i32
+// }
+
+fn main() {
+    // let coord = Point { x: 5.0, y: 5.0 };
+    // let coord2 = Point { x: 'x', y: 'y' };
+    // let coord3 = Point {
+    //     x: 'x',
+    //     y: String::from("LOL"),
+    // };
+}
