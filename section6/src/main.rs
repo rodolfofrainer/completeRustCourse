72fbcdc6ba7a0c29efedfbc4d29a6f5511e22e6c
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-06 11:10:21 +0100

    Section6- Traits

--- section6/src/main.rs
+++ section6/src/main.rs
@@ -4,6 +4,31 @@
 //     y: U, //i32
 // }
 
+// Traits
+trait Overview {
+    fn overview(&self) -> String {
+        String::from("This is a rust course")
+    }
+}
+
+struct Course {
+    headline: String,
+    author: String,
+}
+
+struct AnotherCourse {
+    headline: String,
+    author: String,
+}
+
+impl Overview for Course {}
+
+impl Overview for AnotherCourse {
+    fn overview(&self) -> String {
+        format!("{}, {}", self.author, self.headline)
+    }
+}
+
 fn main() {
     // let coord = Point { x: 5.0, y: 5.0 };
     // let coord2 = Point { x: 'x', y: 'y' };
@@ -11,4 +36,17 @@ fn main() {
     //     x: 'x',
     //     y: String::from("LOL"),
     // };
+
+    let course1 = Course {
+        headline: String::from("Headline"),
+        author: String::from("Tyler"),
+    };
+
+    let course2 = AnotherCourse {
+        headline: String::from("Headline2"),
+        author: String::from("NotTyler"),
+    };
+
+    println!("{}", course1.overview());
+    println!("{}", course2.overview());
 }
