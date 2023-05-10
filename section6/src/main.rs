7ca913732e8dc0e2547f5f5effc2fe25160c1768
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-10 09:34:45 +0100

    section 6 - operator overload

--- section6/src/main.rs
+++ section6/src/main.rs
@@ -36,7 +36,34 @@ impl Overview for AnotherCourse {
     }
 }
 
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Add for Point<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
 fn main() {
+    let coord = Point { x: 5.0, y: 5.0 };
+    let coord2 = Point { x: 1.0, y: 2.0 };
+
+    let sum = coord + coord2;
+    println!("{:?}", sum);
+
     // let coord = Point { x: 5.0, y: 5.0 };
     // let coord2 = Point { x: 'x', y: 'y' };
     // let coord3 = Point {
