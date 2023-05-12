2456c9fa27505900ae7238f4984f66a1394117db
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-12 16:29:38 +0100

    section 2 - variable and mutables

---
+++ section2/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let mut x = 5;
+
+    println!("The value of x is: {}", x);
+
+    x = 10;
+    println!("The value of x is: {}", x);
+
+    const SECONDS: i8 = 60;
+    println!("There are {} seconds in a minute", SECONDS);
+}
