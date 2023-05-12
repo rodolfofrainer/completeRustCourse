f89fbbe8a43b782409456d9e87d71ae5e23331bb
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-12 17:25:52 +0100

    section 2 - arrays

--- section2/data_types/src/main.rs
+++ section2/data_types/src/main.rs
@@ -1,52 +1,80 @@
 fn main() {
-    // let x: i8 = -10;
-    // println!("The value of x is: {}", x);
-    // let y: u8 = 10; // cant be negative
-    // println!("The value of y is: {}", y);
+    // // let x: i8 = -10;
+    // // println!("The value of x is: {}", x);
+    // // let y: u8 = 10; // cant be negative
+    // // println!("The value of y is: {}", y);
 
-    // let decimal = 02_55;
-    // let hex = 0xff;
-    // let octal = 0o377;
-    // let binary = 0b1111_1111;
+    // // let decimal = 02_55;
+    // // let hex = 0xff;
+    // // let octal = 0o377;
+    // // let binary = 0b1111_1111;
 
-    // println!("decimal: {}", decimal);
-    // println!("hex: {}", hex);
-    // println!("octal: {}", octal);
-    // println!("binary: {}", binary);
+    // // println!("decimal: {}", decimal);
+    // // println!("hex: {}", hex);
+    // // println!("octal: {}", octal);
+    // // println!("binary: {}", binary);
 
-    // let byte = b'A';
-    // println!("byte: {}", byte);
+    // // let byte = b'A';
+    // // println!("byte: {}", byte);
 
-    let x = 2.0; // f64 default
-    let y: f32 = 1.0;
+    // let x = 2.0; // f64 default
+    // let y: f32 = 1.0;
 
-    println!("x: {}", x);
-    println!("y: {}", y);
+    // println!("x: {}", x);
+    // println!("y: {}", y);
 
-    let t = true;
-    let f = false;
+    // let t = true;
+    // let f = false;
 
-    println!("t: {}", t);
-    println!("f: {}", f);
+    // println!("t: {}", t);
+    // println!("f: {}", f);
 
-    let c = 'c';
+    // let c = 'c';
 
-    println!("c: {}", c);
+    // println!("c: {}", c);
 
-    // + - * / %
+    // // + - * / %
 
-    let a = 10;
-    let b = 3;
+    // let a = 10;
+    // let b = 3;
 
-    let remainder = a % b;
-    let sum = a + b;
-    let minus = a - b;
-    let multiply = a * b;
-    let division = a / b;
+    // let remainder = a % b;
+    // let sum = a + b;
+    // let minus = a - b;
+    // let multiply = a * b;
+    // let division = a / b;
 
-    println!("remainder: {}", remainder);
-    println!("sum: {}", sum);
-    println!("minus: {}", minus);
-    println!("multiply: {}", multiply);
-    println!("division: {}", division);
+    // println!("remainder: {}", remainder);
+    // println!("sum: {}", sum);
+    // println!("minus: {}", minus);
+    // println!("multiply: {}", multiply);
+    // println!("division: {}", division);
+
+    //TUPLES
+
+    // let tup = (50, "hi", true);
+
+    // println!("tup: {:?}", tup);
+    // println!("x: {}", tup.0);
+    // println!("y: {}", tup.1);
+    // println!("z: {}", tup.2);
+
+    // let (x, y, z) = tup;
+
+    // println!("x: {}", x);
+    // println!("y: {}", y);
+    // println!("z: {}", z);
+
+    // ARRAYS
+
+    // let array = [1, 2, 3];
+    // for i in array {
+    //     println!("i: {}", i);
+    // }
+
+    // let mut array2: [i32; 3] = [4, 5, 6];
+    // println!("array2: {:?}", array2);
+    // array2[0] = 10;
+    // println!("array2: {:?}", array2);
+    // array2[3] = 1;
 }
