7743172fc4b3db6405f39ffd1478e37ad7c0ca52
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-13 21:26:14 +0100

    section 3 - move

--- section3/ownership/src/main.rs
+++ section3/ownership/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    let var = 1;
+    // let var = 1;
 
-    let mut s = "hello".to_string(); // created on the heap
+    // let mut s = "hello".to_string(); // created on the heap
 
-    s.push_str(", world!");
-    println!("{}", s);
+    // s.push_str(", world!");
+    // println!("{}", s);
+
+    let x = vec!["tyler".to_string()];
+    let y = x;
+
+    // println!("{:?}", x); // produces error: borrow of moved value: `x`
 }
 
 // println!("{}", s); // produces an error
