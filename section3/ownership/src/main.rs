3c7fa637a35110d5aec4f52668ce2d7bef4c1ac3
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-13 21:29:42 +0100

    section 3 - copy

--- section3/ownership/src/main.rs
+++ section3/ownership/src/main.rs
@@ -6,12 +6,16 @@ fn main() {
     // s.push_str(", world!");
     // println!("{}", s);
 
-    let x = vec!["tyler".to_string()];
-    let y = x.clone();
-    let z = y.clone();
-    println!("{:?}", x);
-    println!("{:?}", y);
-    println!("{:?}", z);
+    // let x = vec!["tyler".to_string()];
+    // let y = x.clone();
+    // let z = y.clone();
+    // println!("{:?}", x);
+    // println!("{:?}", y);
+    // println!("{:?}", z);
+
+    let x = 1;
+    let y = x; //implements copy trait
+    println!("x: {}, y: {}", x, y);
 }
 
 // println!("{}", s); // produces an error
