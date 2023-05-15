889b3a7b90448d63f85d0ca97867b432f6a9db61
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2023-05-15 15:20:55 +0100

    section 5 - methods

--- section4/structs/src/main.rs
+++ section4/structs/src/main.rs
@@ -1,31 +1,60 @@
-struct User {
-    active: bool,
-    username: String,
-    sign_in_count: u32,
+// struct User {
+//     active: bool,
+//     username: String,
+//     sign_in_count: u32,
+// }
+
+// struct Coordinates(i32, i32, i32);
+
+// struct UnitStruct;
+
+struct Square {
+    width: u32,
+    height: u32,
 }
 
-struct Coordinates(i32, i32, i32);
+impl Square {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 
-struct UnitStruct;
+    fn whats_my_width(&self) -> u32 {
+        self.width
+    }
 
-fn main() {
-    let user1 = User {
-        active: true,
-        username: "Lol".to_string(),
-        sign_in_count: 1,
-    };
-    println!("{}", user1.username);
-
-    let user2 = build_user(String::from("thisOrThat"));
-    println!("{}", user2.username);
-
-    let coords = Coordinates(1, 2, 3);
-}
-
-fn build_user(username: String) -> User {
-    User {
-        username,
-        active: true,
-        sign_in_count: 1,
+    fn change_width(&mut self, width: u32) {
+        self.width = width;
     }
 }
+
+fn main() {
+    //     let user1 = User {
+    //         active: true,
+    //         username: "Lol".to_string(),
+    //         sign_in_count: 1,
+    //     };
+    //     println!("{}", user1.username);
+
+    //     let user2 = build_user(String::from("thisOrThat"));
+    //     println!("{}", user2.username);
+
+    //     let coords = Coordinates(1, 2, 3);
+    // }
+
+    let mut square = Square {
+        width: 5,
+        height: 5,
+    };
+
+    println!("{}", square.area());
+    println!("{}", square.whats_my_width());
+    square.change_width(32);
+    println!("{}", square.whats_my_width());
+}
+
+// fn build_user(username: String) -> User {
+//     User {
+//         username,
+//         active: true,
+//         sign_in_count: 1,
+//     }
